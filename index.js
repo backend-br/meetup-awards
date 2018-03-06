5426ae522c584cd991fdf20026f44a6d2f58a549
Matheus Hernandes <midia.matheus@gmail.com>
2018-03-05 22:54:50 -0300

    add some doc to methods, fix getConfirmedMembers, add sort method, add env-example.json, fix example within index.js

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
-const Twitter = require('./twitter')
 const Meetup = require('./meetup')
-
-// const tt = new Twitter('backendbrasil')
 const mt = new Meetup('backendbrasil')
 
-// console.log(tt.getPostAbout())
-memberWin = mt.getMemberConfirmeds()
-
-console.log(memberWin)
+mt.getConfirmedMembers()
+  .then(mt.sort)
+  .then(console.log)
+  .catch(console.error)
